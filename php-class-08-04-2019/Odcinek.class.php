3ccb240134d173cc1565cfecc1b33c0179e217b4
dominikoso <rodzinka.kostkow@gmail.com>
2019-04-09 19:18:48 +0200

    php-class-08-04-2019 - Moved drawing to class

--- php-class-08-04-2019/Odcinek.class.php
+++ php-class-08-04-2019/Odcinek.class.php
@@ -11,10 +11,10 @@
          */
         public function __construct( $x1=null, $y1=null, $x2=null, $y2=null )
         { 
-            $this->x1 = ($x1 != null && is_int($x1)) ? $x1 : random_int(-100, 100);
-            $this->x2 = ($x2 != null && is_int($x2)) ? $x2 : random_int(-100, 100);
-            $this->y1 = ($y1 != null && is_int($y1)) ? $y1 : random_int(-100, 100);
-            $this->y2 = ($y2 != null && is_int($y2)) ? $y2 : random_int(-100, 100);
+            $this->x1 = ($x1 != null && is_int($x1) && $x1 <= 100 && $x1 >= -100) ? $x1 : random_int(-100, 100);
+            $this->x2 = ($x2 != null && is_int($x2) && $x2 <= 100 && $x2 >= -100) ? $x2 : random_int(-100, 100);
+            $this->y1 = ($y1 != null && is_int($y1) && $y1 <= 100 && $y1 >= -100) ? $y1 : random_int(-100, 100);
+            $this->y2 = ($y2 != null && is_int($y2) && $y2 <= 100 && $y2 >= -100) ? $y2 : random_int(-100, 100);
         }
 
         function liczDlugosc()
@@ -24,6 +24,12 @@
             if ($this->x1 == $this->x2 && $this->y1 == $this->y2){ return 0; }
             else{ return $distance = ( sqrt($x + $y) ); }
 
+        }
+
+        function drawLine($handle)
+        {
+            $line_color = ImageColorAllocate ($handle, random_int(0,255), random_int(0, 255), random_int(0,255));
+            ImageLine($handle, $this->x1+100, $this->y1+100, $this->x2+100, $this->y2+100, $line_color);
         }
 
         public function getX1 () {
